56e659689c1f2505ceb7fd4acf8d07eb4747e7e0
Asmat Soomro <soomro.asmat@gmail.com>
2021-02-14 23:56:49 -0500

    Added load balancer between campaigns

--- src/main/java/com/app/campaignNavigator/service/CampaignService.java
+++ src/main/java/com/app/campaignNavigator/service/CampaignService.java
@@ -21,42 +21,71 @@ public class CampaignService {
     @Qualifier("campaign")
     private Set<Campaign> campaignSet;
 
-    public String getAssociatedSegments(String[] desiredSegments){
+    Map<String, Integer> segementLoad = new HashMap<>();
+
+
+    public String getAssociatedSegments(String[] desiredSegments) {
+        List<String> maxCampaigns = new ArrayList<>();
         desiredSegments = ignoreDuplicateElements(desiredSegments);
-        int max =0;
+        int max = 0;
         String largestAssociatedSegment = NO_CAMPAIGN;
         Iterator<Campaign> itr = campaignSet.iterator();
-        while (itr.hasNext()){
+        while (itr.hasNext()) {
             Campaign campaign = itr.next();
             logger.info("campaign:: {}", campaign.getName());
             String[] campaignSegments = campaign.getSegments();
             int identicalElements = 0;
-            for (int i=0; i<campaignSegments.length; i++){
-                for (int j=0; j<desiredSegments.length; j++){
+            for (int i = 0; i < campaignSegments.length; i++) {
+                for (int j = 0; j < desiredSegments.length; j++) {
 
-                    if (campaignSegments[i].equals(desiredSegments[j])){
+                    if (campaignSegments[i].equals(desiredSegments[j])) {
                         identicalElements++;
                     }
 
                 }
             }
-            if (identicalElements >= max && identicalElements!=0){
+            if (identicalElements >= max && identicalElements != 0) {
+                maxCampaigns.add(campaign.getName());
                 max = identicalElements;
                 largestAssociatedSegment = campaign.getName();
             }
+
             logger.info("Number of identical elements for campaign {} == {}, maximum = {}", campaign.getName(), identicalElements, max);
         }
 
+
         logger.info("max {}", max);
+        if (largestAssociatedSegment != NO_CAMPAIGN) {
+            largestAssociatedSegment = balanceLoadBetweenCampaigns(maxCampaigns, largestAssociatedSegment);
+        }
+        return largestAssociatedSegment;
+    }
+
+    private String balanceLoadBetweenCampaigns(List<String> maxCampaigns, String largestAssociatedSegment) {
+        int min = segementLoad.get(maxCampaigns.get(0)) != null ? segementLoad.get(maxCampaigns.get(0)) : 0;
+        for (int i = 0; i < maxCampaigns.size(); i++) {
+            Integer loadVal = segementLoad.get(maxCampaigns.get(i));
+            if (loadVal == null) {
+                loadVal = 0;
+            }
+            if (loadVal <= min) {
+                min = loadVal;
+                largestAssociatedSegment = maxCampaigns.get(i);
+            }
+
+
+        }
+        int count = segementLoad.containsKey(largestAssociatedSegment) ? segementLoad.get(largestAssociatedSegment) : 0;
+        segementLoad.put(largestAssociatedSegment, ++count);
         return largestAssociatedSegment;
     }
 
     // duplicate elements to be completely ignored, i.e. {"3", "5", "5"} -> {"3"}
-    public String[] ignoreDuplicateElements(String[] arr){
+    public String[] ignoreDuplicateElements(String[] arr) {
         LinkedHashSet<String> linkedHashSet = new LinkedHashSet<>();
-        for (int i=0; i<arr.length;i++){
-            for (int j=i+1;j<arr.length;j++){
-                if (arr[i].equals(arr[j])){
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i].equals(arr[j])) {
                     arr[i] = null;
                     arr[j] = null;
 
@@ -65,8 +94,8 @@ public class CampaignService {
         }
 
 
-        for (int i=0; i<arr.length; i++){
-            if (arr[i]!=null){
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != null) {
                 linkedHashSet.add(arr[i]);
             }
         }
